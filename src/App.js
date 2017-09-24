7e80113da18bd7d6e0ccf252680a04b56bcc9599
Cerberus <shadow_sun0708@hotmail.com>
2017-09-24 16:53:57 +0700

    Add Create Note
    but not update real time, refresh to see new note

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import 'bulma/css/bulma.css'
 import NavBar from './NavBar'
 import NoteList from './NoteList'
+import CreateNote from './CreateNote'
 
 const App = () => (
   <NavBar>
     <NoteList />
+    <CreateNote />
   </NavBar>
 )
 
---
+++ src/CreateNote.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { graphql, gql } from 'react-apollo'
+import { compose, withState, withHandlers } from 'recompose'
+
+const CREATE_NOTE = gql`
+  mutation($detail: String!) {
+    createNote(detail: $detail) {
+      id
+    }
+  }
+`
+
+const enhance = compose(
+  withState('detail', 'setDetail', ''),
+  graphql(CREATE_NOTE, {
+    name: 'createNoteQuery',
+  }),
+  withHandlers({
+    submit: ({ detail, setDetail, createNoteQuery }) => () => {
+      createNoteQuery({
+        variables: {
+          detail,
+        },
+      })
+        .then(() => setDetail('')) // reset form
+        .catch(err => console.error(err))
+    },
+  }),
+)
+
+const CreateNote = ({ detail, setDetail, submit }) => (
+  <div className="columns">
+    <div className="column is-half is-offset-one-quarter">
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          submit()
+        }}
+        className="field is-grouped"
+      >
+        <input
+          type="text"
+          className="input"
+          onChange={e => setDetail(e.target.value)}
+          value={detail}
+        />
+        <button type="submit" className="button">
+          Create
+        </button>
+      </form>
+    </div>
+  </div>
+)
+export default enhance(CreateNote)
