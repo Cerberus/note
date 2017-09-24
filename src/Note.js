3cd0bea6449a824d1335d9e008bfef6d5937271e
Cerberus <shadow_sun0708@hotmail.com>
2017-09-24 17:30:24 +0700

    Add delete button to Note component

--- src/Note.js
+++ src/Note.js
@@ -1,5 +1,32 @@
 import React from 'react'
+import { graphql, gql } from 'react-apollo'
+import { compose } from 'recompose'
 
-const Note = ({ detail }) => <li>{detail}</li>
+const DELETE_NOTE = gql`
+  mutation($id: ID!) {
+    deleteNote(id: $id) {
+      id
+    }
+  }
+`
 
-export default Note
+const enhance = compose(
+  graphql(DELETE_NOTE, {
+    name: 'deleteLink',
+    options: ({ id }) => ({
+      variables: { id },
+    }),
+  }),
+)
+const Note = ({ detail, deleteLink }) => (
+  <li>
+    {detail}{' '}
+    <a // eslint-disable-line jsx-a11y/anchor-has-content
+      className="delete is-small"
+      onClick={deleteLink}
+      style={{ marginTop: '5px' }}
+    />
+  </li>
+)
+
+export default enhance(Note)
