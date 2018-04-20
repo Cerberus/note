15ef2e9946b9c1d0e71446db6dc55b2356fef524
Thanaphol Pomsuwan <thanapol@wongnai.com>
2018-04-21 04:44:00 +0700

    auto focus after submit

--- src/CreateNote/index.js
+++ src/CreateNote/index.js
@@ -4,7 +4,7 @@ import gql from 'graphql-tag'
 import { compose, withState, withHandlers } from 'recompose'
 
 const CREATE_NOTE = gql`
-  mutation CreateNoteM ($detail: String!) {
+  mutation CreateNoteM($detail: String!) {
     createNote(detail: $detail) {
       id
     }
@@ -12,44 +12,58 @@ const CREATE_NOTE = gql`
 `
 
 const enhance = compose(
+  withState('isSubmitting', 'setIsSubmitting', false),
   withState('detail', 'setDetail', ''),
   graphql(CREATE_NOTE, {
     name: 'createNoteQuery',
   }),
   withHandlers({
-    submit: ({ detail, setDetail, createNoteQuery }) => () => {
-      createNoteQuery({
+    submit: ({ detail, setDetail, createNoteQuery, setIsSubmitting }) => () => {
+      setIsSubmitting(true)
+      return createNoteQuery({
         variables: {
           detail,
         },
       })
         .then(() => setDetail('')) // reset form
         .catch(err => console.error(err))
+        .then(() => setIsSubmitting(false))
     },
   }),
 )
 
-const CreateNote = ({ detail, setDetail, submit }) => (
-  <div className="columns">
-    <div className="column is-half is-offset-one-quarter">
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          submit()
-        }}
-        className="field is-grouped"
-      >
-        <input
-          type="text"
-          className="input"
-          onChange={e => setDetail(e.target.value)}
-          value={detail}
-        />
-        <button type="submit" className="button">
-          Create
-        </button>
-      </form>
-    </div>
-  </div>
-)
+class CreateNote extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.textInput = React.createRef()
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+    this.props.submit().then(() => this.textInput.current.focus())
+  }
+
+  render() {
+    const { detail, setDetail, isSubmitting } = this.props
+    return (
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <form onSubmit={this.onSubmit} className="field is-grouped">
+            <input
+              type="text"
+              className="input"
+              onChange={e => setDetail(e.target.value)}
+              disabled={isSubmitting}
+              value={detail}
+              ref={this.textInput}
+            />
+            <button type="submit" className="button">
+              Create
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
 export default enhance(CreateNote)
